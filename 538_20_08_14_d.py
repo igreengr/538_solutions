4ca3e972ff699a677e42c8c2002ab3f9a9a85c20
igreengr <40578933+igreengr@users.noreply.github.com>
2020-08-16 18:27:53 -0500

    Add files via upload
    This is the best one. The average length of the section of the ruler with 6 in it is: 5.625 inches (5 5/8)

--- 538_20_08_14_d.py
+++ 538_20_08_14_d.py
@@ -4,20 +4,18 @@ tests = 1000000
 six_len = 0
 
 for a in range(tests):
-    ruler = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    cut_one = random.uniform(0, 1) * 12
-    cut_two = random.uniform(0, 1) * 12
-    cut_three = random.uniform(0, 1) * 12
+    cut_one = random.uniform(0, 12)
+    cut_two = random.uniform(0, 12)
+    cut_three = random.uniform(0, 12)
     while cut_one == cut_two:
-        cut_two = random.uniform(0, 1) * 12
+        cut_two = random.uniform(0, 12)
     while cut_one == cut_three:
-        cut_three = random.uniform(0, 1) * 12
+        cut_three = random.uniform(0, 12)
     while cut_two == cut_three:
-        cut_three = random.uniform(0, 1) * 12
+        cut_three = random.uniform(0, 12)
 
     cuts = [cut_one, cut_two, cut_three]
     cuts.sort()
-
 
     ruler_one = [cuts[2], 12]
     ruler_two = [cuts[1], cuts[2]]
